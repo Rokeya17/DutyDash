18a25bce7bb4b8bf283af5bb0ed1795ea77e5739
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-20 22:46:22 +0600

    themedata ui screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
   runApp(const DutyDash());
-}
-
-class DutyDash extends StatelessWidget {
-  const DutyDash({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Task Manager app',
-      theme: ThemeData(brightness: Brightness.light),
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      themeMode: ThemeMode.light,
-    );
-  }
 }
