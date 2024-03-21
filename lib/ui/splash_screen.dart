bb3e529fc9b9f9ec4235585f39d02bd7061abbbb
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-21 23:43:22 +0600

    splash ui screen

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: SvgPicture.asset(
-              'assets/images/Background.svg',
-              fit: BoxFit.fitHeight,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: SvgPicture.asset(
+                'assets/images/Background.svg',
+                fit: BoxFit.fitHeight,
+              ),
             ),
-          ),
-          Center(
-            child: SvgPicture.asset(
-              'assets/images/logo.svg',
-              height: 200,
+            Center(
+              child: SvgPicture.asset(
+                'assets/images/logo.svg',
+                height: 200,
+              ),
             ),
-          ),
-        ],
+            const Spacer(),
+            const CircularProgressIndicator(
+              color: Colors.black,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Text('Version 1.0')
+          ],
+        ),
       ),
     );
   }
