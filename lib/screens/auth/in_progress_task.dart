85a8fc31caa2f5daf193b9ecec5252eb2f069b1d
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-23 22:57:14 +0600

    splash ui screen

---
+++ lib/screens/auth/in_progress_task.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InProgressTask extends StatefulWidget {
+  const InProgressTask({super.key});
+
+  @override
+  State<InProgressTask> createState() => _InProgressTaskState();
+}
+
+class _InProgressTaskState extends State<InProgressTask> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
