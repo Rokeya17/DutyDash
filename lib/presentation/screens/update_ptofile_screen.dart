444994d7260a91b811ea5b619d7ad572122e9c43
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-26 23:13:58 +0600

    update profile ui screen added

--- lib/presentation/screens/update_ptofile_screen.dart
+++ lib/presentation/screens/update_ptofile_screen.dart
@@ -12,11 +12,83 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'Update Profile',
-            style: TextStyle(fontWeight: FontWeight.w800),
-          )
+            style: TextStyle(
+                fontWeight: FontWeight.w800, color: Colors.blue, fontSize: 25),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  label: const Text(
+                    'email',
+                    style: TextStyle(fontSize: 16),
+                  )),
+            ),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                label: const Text(
+                  'First Name',
+                  style: TextStyle(fontSize: 16),
+                )),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                label: const Text(
+                  'Last Name',
+                  style: TextStyle(fontSize: 16),
+                )),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                label: const Text(
+                  'Phone Number',
+                  style: TextStyle(fontSize: 16),
+                )),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              label: const Text(
+                'Password',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                label: const Text(
+                  'email',
+                  style: TextStyle(fontSize: 16),
+                )),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                label: const Text(
+                  'email',
+                  style: TextStyle(fontSize: 16),
+                )),
+          ),
         ],
       ),
     );
