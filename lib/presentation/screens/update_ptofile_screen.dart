5684aab453d5fdaea3db62c031c83af2195c833c
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-26 23:23:28 +0600

    update profile ui screen added

--- lib/presentation/screens/update_ptofile_screen.dart
+++ lib/presentation/screens/update_ptofile_screen.dart
@@ -11,85 +11,121 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Update Profile',
-            style: TextStyle(
-                fontWeight: FontWeight.w800, color: Colors.blue, fontSize: 25),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-              decoration: InputDecoration(
+      appBar: AppBar(
+        leading: const BackButton(),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Update Profile',
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    color: Colors.blue,
+                    fontSize: 25),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    label: const Text(
+                      'email',
+                      style: TextStyle(fontSize: 16),
+                    )),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    label: const Text(
+                      'First Name',
+                      style: TextStyle(fontSize: 16),
+                    )),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    label: const Text(
+                      'Last Name',
+                      style: TextStyle(fontSize: 16),
+                    )),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    label: const Text(
+                      'Phone Number',
+                      style: TextStyle(fontSize: 16),
+                    )),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10)),
                   label: const Text(
-                    'email',
+                    'Password',
                     style: TextStyle(fontSize: 16),
-                  )),
-            ),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                label: const Text(
-                  'First Name',
-                  style: TextStyle(fontSize: 16),
-                )),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                label: const Text(
-                  'Last Name',
-                  style: TextStyle(fontSize: 16),
-                )),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                label: const Text(
-                  'Phone Number',
-                  style: TextStyle(fontSize: 16),
-                )),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              label: const Text(
-                'Password',
-                style: TextStyle(fontSize: 16),
+                  ),
+                ),
               ),
-            ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    label: const Text(
+                      'email',
+                      style: TextStyle(fontSize: 16),
+                    )),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    label: const Text(
+                      'email',
+                      style: TextStyle(fontSize: 16),
+                    )),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Update'),
+                ),
+              )
+            ],
           ),
-          TextFormField(
-            decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                label: const Text(
-                  'email',
-                  style: TextStyle(fontSize: 16),
-                )),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                label: const Text(
-                  'email',
-                  style: TextStyle(fontSize: 16),
-                )),
-          ),
-        ],
+        ),
       ),
     );
   }
