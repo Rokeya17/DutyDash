a86d593a1063a0e47bd9835953c7c5d94e07bc44
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-28 22:44:41 +0600

    update profile screen

--- lib/presentation/screens/pin_verification_screen.dart
+++ lib/presentation/screens/pin_verification_screen.dart
@@ -1,42 +1,44 @@
-import 'package:dutydash/presentation/screens/create_accountscreen.dart';
-import 'package:dutydash/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+import 'create_accountscreen.dart';
+import 'home_screen.dart';
+
+class PinVerificationScreen extends StatefulWidget {
+  const PinVerificationScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<PinVerificationScreen> createState() => _PinVerificationScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _emailEditingController = TextEditingController();
-  final TextEditingController _passwordEditingController =
-      TextEditingController();
-  bool _passwordVisibility = false;
-
+class _PinVerificationScreenState extends State<PinVerificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: const BackButton(
-          color: Colors.black,
-        ),
-      ),
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            AppBar(
+              title: const Text(
+                'Get Started with',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+              leading: const BackButton(
+                color: Colors.black,
+              ),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
                   TextFormField(
-                    controller: _emailEditingController,
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
@@ -48,29 +50,6 @@ class _LoginScreenState extends State<LoginScreen> {
                           'Enter your email', // Use labelText instead of label
                       suffixIcon: const Icon(Icons.email_outlined),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  TextFormField(
-                    controller: _passwordEditingController,
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: const BorderSide(color: Colors.blue),
-                        ),
-                        labelText: 'Password',
-                        suffixIcon: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _passwordVisibility = !_passwordVisibility;
-                              });
-                            },
-                            icon: _passwordVisibility
-                                ? const Icon(Icons.visibility)
-                                : const Icon(Icons.visibility_off))),
                   ),
                   const SizedBox(
                     height: 16,
