d8a13659d0a62a4b3fef3af889b924961f97beb6
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-31 23:49:08 +0600

    Email and OTP verification UI screen added

--- lib/presentation/screens/email_verification_screen.dart
+++ lib/presentation/screens/email_verification_screen.dart
@@ -9,11 +9,67 @@ class EmailVerificationScreen extends StatefulWidget {
 }
 
 class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [Text('Your Email Address')],
+        body: Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Your Email Address',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 33,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const Text(
+                'A 6 digits PIN will sent to your email address',
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                    color: Colors.grey),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                controller: _emailTextEditingController,
+                decoration: InputDecoration(
+                  label: const Text('email'),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                height: 30,
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {}, child: const Icon(Icons.arrow_forward)),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Have an account?'),
+                  TextButton(onPressed: () {}, child: const Text('signIn'))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     ));
   }
 }
--- lib/presentation/screens/signin_screen.dart
+++ lib/presentation/screens/signin_screen.dart
@@ -27,103 +27,108 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
       body: SafeArea(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 16),
-            child: Text(
-              'Get started with ',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w500,
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: Text(
+                'Get started with ',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _emailEditingController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: const BorderSide(color: Colors.blue),
-                    ),
-                    labelText: 'Enter your email',
-                    suffixIcon: const Icon(Icons.email_outlined),
-                  ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                TextFormField(
-                  controller: _passwordEditingController,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  decoration: InputDecoration(
+            const SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _emailEditingController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10.0),
                         borderSide: const BorderSide(color: Colors.blue),
                       ),
-                      labelText: 'Password',
-                      suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _passwordVisibility = !_passwordVisibility;
-                            });
-                          },
-                          icon: _passwordVisibility
-                              ? const Icon(Icons.visibility)
-                              : const Icon(Icons.visibility_off))),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                  height: 30,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Get.to(const Homescreen());
-                    },
-                    child: const Icon(Icons.arrow_forward_ios),
+                      labelText: 'Enter your email',
+                      suffixIcon: const Icon(Icons.email_outlined),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: _passwordEditingController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: const BorderSide(color: Colors.blue),
+                        ),
+                        labelText: 'Password',
+                        suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _passwordVisibility = !_passwordVisibility;
+                              });
+                            },
+                            icon: _passwordVisibility
+                                ? const Icon(Icons.visibility)
+                                : const Icon(Icons.visibility_off))),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    height: 30,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Get.to(const Homescreen());
+                      },
+                      child: const Icon(Icons.arrow_forward_ios),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: const Text(
+                      'Forget Password?',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have an account?"),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      Get.to(const SignUpScreen());
+                    });
+                  },
                   child: const Text(
-                    'Forget Password?',
-                    style: TextStyle(color: Colors.grey),
+                    'Sign Up',
+                    style: TextStyle(color: Colors.blue),
                   ),
                 ),
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Don't have an account?"),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    Get.to(const SignUpScreen());
-                  });
-                },
-                child: const Text('SignUp'),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       )),
     );
   }
