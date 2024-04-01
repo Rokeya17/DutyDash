1828513b47be47e5589cd0958fa0501b13da9aac
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-04-01 23:56:03 +0600

    BottomNav UI screen added

--- lib/presentation/screens/bottomnav_base_screen.dart
+++ lib/presentation/screens/bottomnav_base_screen.dart
@@ -1,24 +1,43 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavScreen extends StatefulWidget {
   const BottomNavScreen({super.key});
 
   @override
-  State<BottomNavScreen> createState() => _BottomNavScreenState();
+  _BottomNavScreenState createState() => _BottomNavScreenState();
 }
 
 class _BottomNavScreenState extends State<BottomNavScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const Placeholder(color: Colors.blueGrey),
+    const Placeholder(color: Colors.blueGrey),
+    const Placeholder(color: Colors.blueGrey),
+    const Placeholder(color: Colors.blueGrey),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blue,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.list)),
-          BottomNavigationBarItem(icon: Icon(Icons.list)),
-          BottomNavigationBarItem(icon: Icon(Icons.list)),
-          BottomNavigationBarItem(icon: Icon(Icons.list)),
+      backgroundColor: Colors.blueGrey,
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.blueGrey,
+        items: <Widget>[
+          const Icon(Icons.list, size: 30),
+          const Icon(Icons.lock_clock_outlined, size: 30),
+          const Icon(Icons.delete, size: 30),
+          const Icon(Icons.done_all, size: 30),
         ],
+        onTap: _onItemTapped,
       ),
     );
   }
--- lib/presentation/utility/asset_utility.dart
+++ lib/presentation/utility/asset_utility.dart
@@ -2,5 +2,9 @@ class AssetUtility {
   AssetUtility._(); //use for Constant images
   static const images = 'assets/images';
   static const String logoSVG = 'assets/images/logo.svg';
-  static const String backgroundSVG = 'Background.svg';
+  static const String backgroundSVG = 'assets/images/Background.svg';
+  static const String listSVG = 'assets/images/list.svg';
+  static const String inprogressSVG = 'assets/images/in-progress.svg';
+  static const String deleteSVG = 'assets/images/delete.svg';
+  static const String doneSVG = 'assets/images/done.svg';
 }
