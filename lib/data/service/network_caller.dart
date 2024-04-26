529bd3c6522720725baf6d8f0536a4717a36c1d3
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-04-26 23:08:14 +0600

    Network caller has been modified with response_object

--- lib/data/service/network_caller.dart
+++ lib/data/service/network_caller.dart
@@ -1,14 +1,23 @@
 import 'dart:convert';
+import 'dart:core';
+import 'dart:developer';
 
-import 'package:get/get.dart';
+import 'package:dutydash/data/models/response_object.dart';
 import 'package:http/http.dart';
 
 class Networkcaller {
-  Future<void> getRequest(String url) async {
-    Response response = await get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
-      return decodedResponse;
-    } else {}
+  Future<NetworkResponse> getRequest(String url) async {
+    try {
+      Response response = await get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+            true, response.statusCode, jsonDecode(response.body));
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(false, -1, null);
   }
 }
