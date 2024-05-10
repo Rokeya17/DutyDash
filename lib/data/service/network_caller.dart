7dc72bb403696af42d3000770005436a936f79f7
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-05-10 23:59:05 +0600

    network caller has been modified wi

--- lib/data/service/network_caller.dart
+++ lib/data/service/network_caller.dart
@@ -1,16 +1,66 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:dutydash/data/models/response_object.dart';
+import 'package:dutydash/presentation/screens/signup_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
-class Networkcaller {
-  Future<NetworkResponse> getRequest(String url) async {
+import '../models/response_object.dart';
+
+class Dutydash {
+  static GlobalKey<NavigatorState> globalKey = GlobalKey<NavigatorState>();
+}
+
+class NetworkCaller {
+  Future<NetworkResponse> getRequest(String url, {bool isLogin = false}) async {
     try {
-      Response response = await get(Uri.parse(url));
+      Response response = await get(Uri.parse(url),
+          headers: {'token': AuthController.accessToken.toString()});
+      log(response.statusCode.toString());
+      log(response.body);
       if (response.statusCode == 200) {
         return NetworkResponse(
             true, response.statusCode, jsonDecode(response.body));
+      } else if (response.statusCode == 401) {
+        if (isLogin) {
+          await gotoLogin();
+        }
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(
+      false,
+      -1,
+      null,
+    );
+  }
+
+  Future<NetworkResponse> postRequest(String url, Map<String, dynamic> body,
+      {bool isLogin = false}) async {
+    try {
+      Response response = await post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'token': AuthController.accessToken.toString()
+        },
+        body: jsonEncode(body),
+      );
+      log(response.statusCode.toString());
+      log(response.body);
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+          true,
+          response.statusCode,
+          jsonDecode(response.body),
+        );
+      } else if (response.statusCode == 401) {
+        if (!isLogin) {
+          await gotoLogin();
+        }
       } else {
         return NetworkResponse(false, response.statusCode, null);
       }
@@ -20,21 +70,12 @@ class Networkcaller {
     return NetworkResponse(false, -1, null);
   }
 
-  Future<NetworkResponse> postRequest(
-      String url, Map<String, dynamic> body) async {
-    try {
-      Response response = await post(Uri.parse(url),
-          headers: {'Content-Type': 'application/json'},
-          body: jsonEncode(body));
-      if (response.statusCode == 200) {
-        return NetworkResponse(
-            true, response.statusCode, jsonDecode(response.body));
-      } else {
-        return NetworkResponse(false, response.statusCode, null);
-      }
-    } catch (e) {
-      log(e.toString());
-    }
-    return NetworkResponse(false, -1, null);
+  Future<void> gotoLogin() async {
+    AuthController.clear();
+    Navigator.pushAndRemoveUntil(
+      Dutydash.globalKey.currentState!.context,
+      MaterialPageRoute(builder: (context) => const SignUpScreen()),
+      (route) => false,
+    );
   }
 }
